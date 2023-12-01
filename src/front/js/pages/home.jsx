323ae5595749931ab86d462478ce963c6c48d002
Santiago <santiagoduartecor@outlook.com>
2023-11-30 23:51:56 -0300

    Upload Template

---
+++ src/front/js/pages/home.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  return <div className="text-center mt-5">Home</div>;
+};
--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,6 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
