3f5b246a89016f75a74e5558f7ec9483784e2e5e
Santiago <santiagoduartecor@outlook.com>
2023-12-03 02:43:11 -0300

    finish configuration render

--- render_build.sh
+++ render_build.sh
@@ -2,17 +2,19 @@
 # exit on error
 set -o errexit
 
-# Instala las dependencias de Node.js y ejecuta la construcción de la aplicación React
+# Save the current FLASK_ENV
+ORIGINAL_FLASK_ENV=$FLASK_ENV
+
+# Set FLASK_ENV to development for the build
+export FLASK_ENV=development
+
+echo "FLASK_ENV is set to: $FLASK_ENV"
+
 npm install
 npm run build
 
-# Instala las dependencias de Python utilizando pipenv
 pipenv install
-
-# Carga las variables de entorno del archivo .env antes de ejecutar cualquier comando
-if [ -f .env ]; then
-  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
-fi
-
-# Ejecuta el comando de actualización (pipenv run upgrade en este caso)
 pipenv run upgrade
+
+# Restore the original FLASK_ENV
+export FLASK_ENV=$ORIGINAL_FLASK_ENV
