0056b9ac579d64e7b406fc695740dc7b105c3455
Santiago <santiagoduartecor@outlook.com>
2023-12-03 02:45:24 -0300

    new configuration

--- render_build.sh
+++ render_build.sh
@@ -2,19 +2,10 @@
 # exit on error
 set -o errexit
 
-# Save the current FLASK_ENV
-ORIGINAL_FLASK_ENV=$FLASK_ENV
-
-# Set FLASK_ENV to development for the build
-export FLASK_ENV=development
-
-echo "FLASK_ENV is set to: $FLASK_ENV"
-
 npm install
 npm run build
 
 pipenv install
 pipenv run upgrade
 
-# Restore the original FLASK_ENV
-export FLASK_ENV=$ORIGINAL_FLASK_ENV
+
